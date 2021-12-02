8e14d39b7d0b06061fbec76c166ef641da049af8
CNikos <nikolaos.choustoulakis@ista.com>
2021-12-02 14:04:48 +0100

    day 2

--- src/main/scala/de/choustoulakis/advent2021/day1/Submarine.scala
+++ src/main/scala/de/choustoulakis/advent2021/day1/Submarine.scala
@@ -1,47 +1,27 @@
 package de.choustoulakis.advent2021.day1
 
-import de.choustoulakis.advent2021.day1.Submarine.{Coordinate, Norm, START, ZERO}
-import de.choustoulakis.advent2021.day2.{Command, Down, Forward, Up}
+import de.choustoulakis.advent2021.day1.Submarine.{Normalizer, START, ZERO}
+import de.choustoulakis.advent2021.day2.*
 
 class Submarine {
 
-  def countTimesDepthChanged(depths: List[Int], normalizer: Norm = ZERO): Int = countTimesDepthChanged {
-    if (normalizer == ZERO)
-      depths
-    else
-      depths.zipWithIndex.map((e, i) => depths.drop(i).take(normalizer).sum)
+  def countTimesDepthChanged(depths: List[Int], normalizer: Normalizer = ZERO): Int = {
+    def countTimesDepthChanged(depths: List[Int]): Int = depths.zip(depths.tail).filter(_ < _).size
+
+    countTimesDepthChanged {
+      normalizer match {
+        case ZERO => depths
+        case _ => depths.sliding(3).map(_.sum).toList;
+      }
+    }
   }
 
   def calculatePosition(commands: List[Command]): Coordinate = commands.foldLeft(START)(_ move _)
-
-  private def countTimesDepthChanged(depths: List[Int]): Int = depths.zip(depths.tail).filter { case (a, b) => a < b }.size
 }
 
 object Submarine {
-  type Norm = 0 | 3
+  type Normalizer = 0 | 3
 
-  case class Coordinate(x: Int, y: Int, depth: Int = 0) {
-    val result = x * y
-    val resultWithAim = x * depth
-
-    def +(coordinate: Coordinate): Coordinate = {
-      val newX = coordinate.x + x
-      val newY = coordinate.y + y
-      val newDepth = coordinate.x * y + depth
-      Coordinate(newX, newY, newDepth)
-    }
-
-    def -(coordinate: Coordinate): Coordinate = {
-      copy(y = y - coordinate.y)
-    }
-
-    def move(command: Command): Coordinate = command match {
-      case Forward(value) => this + Coordinate(value, 0)
-      case Down(value) => this + Coordinate(0, value)
-      case Up(value) => this - Coordinate(0, value)
-    }
-  }
-
-  val ZERO: Norm = 0
+  val ZERO: Normalizer = 0
   val START = Coordinate(0, 0)
 }
---
+++ src/main/scala/de/choustoulakis/advent2021/day2/Coordinate.scala
@@ -0,0 +1,23 @@
+package de.choustoulakis.advent2021.day2
+
+case class Coordinate(x: Int, y: Int, depth: Int = 0) {
+  val result = x * y
+  val resultWithAim = x * depth
+
+  def +(coordinate: Coordinate): Coordinate = {
+    val newX = coordinate.x + x
+    val newY = coordinate.y + y
+    val newDepth = coordinate.x * y + depth
+    Coordinate(newX, newY, newDepth)
+  }
+
+  def -(coordinate: Coordinate): Coordinate = {
+    copy(y = y - coordinate.y)
+  }
+
+  def move(command: Command): Coordinate = command match {
+    case Forward(value) => this + Coordinate(value, 0)
+    case Down(value) => this + Coordinate(0, value)
+    case Up(value) => this - Coordinate(0, value)
+  }
+}
