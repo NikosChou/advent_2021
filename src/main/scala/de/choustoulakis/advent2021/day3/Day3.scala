bbf648c6d8725cd8bba24396716d5b6256c80969
CNikos <nikolaos.choustoulakis@ista.com>
2021-12-03 13:23:37 +0100

    extract class to file

--- src/main/scala/de/choustoulakis/advent2021/day3/Day3.scala
+++ src/main/scala/de/choustoulakis/advent2021/day3/Day3.scala
@@ -1,6 +1,7 @@
 package de.choustoulakis.advent2021.day3
 
 import de.choustoulakis.advent2021.day3.Day3.*
+import de.choustoulakis.advent2021.day3.Binary.*
 
 import java.math.BigInteger
 
@@ -29,7 +30,7 @@ trait Day3 {
       val filtered = rest.filter(bin => bin.bits(position) == bits(position))
       findLastBinary(bitsMapper, filtered, position + 1)
     }
-    
+
     val findOxygen = findLastBinary(_.gamma)
     val findCo2 = findLastBinary(_.epsilon)
     LifeSupport(findOxygen, findCo2)
@@ -37,7 +38,6 @@ trait Day3 {
 }
 
 object Day3 {
-  type Bit = 0 | 1
 
   case class LifeSupport(oxygenRate: Binary, co2Rate: Binary) {
     val rate = (oxygenRate * co2Rate).toDecimal
@@ -46,36 +46,5 @@ object Day3 {
   case class PowerConsumption(gamma: Binary) {
     val epsilon = !gamma
     val calculatePowerConsumption = gamma * epsilon
-  }
-
-  case class Binary(bits: List[Bit]) {
-    private lazy val value = new BigInteger(bits.foldLeft("")(_ + _), 2).intValue()
-    private lazy val stringRepr = Integer.toBinaryString(value)
-    lazy val toDecimal: Int = Integer.parseInt(stringRepr, 2);
-
-    override def toString: String = s"Binary(${bits.foldLeft("")(_ + _)})"
-
-    def findCommonBit(): Bit = bits.foldLeft((0, 0)) {
-      case (acc, 0) => acc.copy(acc._1 + 1, acc._2)
-      case (acc, 1) => acc.copy(acc._1, acc._2 + 1)
-    } match {
-      case (zeros, ones) if ones >= zeros => 1
-      case _ => 0
-    }
-
-
-    def *(other: Binary): Binary = Binary.fromString(Integer.toBinaryString(value * other.value))
-
-    def unary_! : Binary = {
-      def not(bit: Bit): Bit = if (bit == 1) 0 else 1
-
-      Binary(bits.map(not).asInstanceOf[List[Bit]])
-    }
-  }
-
-  object Binary {
-    def fromString(input: String): Binary = Binary(
-      input.split("").map(_.toInt).map(_.asInstanceOf[Bit]).toList.asInstanceOf[List[Bit]]
-    )
   }
 }
