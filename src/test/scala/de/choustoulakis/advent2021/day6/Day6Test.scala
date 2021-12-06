b06cca08bb1620f95d01a4b9a4187f0499912061
Nikolaos Choustoulakis <eppnikos@gmail.com>
2021-12-06 11:39:25 +0200

    Day6 cleaning

--- src/test/scala/de/choustoulakis/advent2021/day6/Day6Test.scala
+++ src/test/scala/de/choustoulakis/advent2021/day6/Day6Test.scala
@@ -1,18 +1,11 @@
 package de.choustoulakis.advent2021.day6
 
 import de.choustoulakis.advent2021.SubmarineSpec
-import org.scalatest.concurrent.Eventually
-import org.scalatest.concurrent.PatienceConfiguration.Timeout
-import org.scalatest.time
-
-import scala.concurrent.Await
-import scala.concurrent.duration.DurationInt
 import scala.io.Source
 
 class Day6Test extends SubmarineSpec with Day6 {
 
   "The Fishes" should {
-    implicit val executionContext = scala.concurrent.ExecutionContext.Implicits.global
     val input = "3,4,3,1,2"
     "grow" in {
       solve(input, 80) shouldBe 5934
