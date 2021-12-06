9f83d04bc689d6923bf42f7e5d3e1cacfd771f9d
Nikolaos Choustoulakis <eppnikos@gmail.com>
2021-12-06 17:53:40 +0200

    Day6 cleaning

--- src/main/scala/de/choustoulakis/advent2021/day6/Day6.scala
+++ src/main/scala/de/choustoulakis/advent2021/day6/Day6.scala
@@ -5,21 +5,24 @@ import de.choustoulakis.advent2021.day6.Day6.Population
 trait Day6:
   def solve(input: String, days: Int): Long =
     val fishes = input.split(",").map(_.toInt)
-    val population = Population(fishes.groupBy(f => f).map(t => (t._1 -> t._2.length.toLong)))
+    val population = Population(fishes.groupBy(identity).map {
+      case (typ, fs) => (typ -> fs.length.toLong)
+    })
 
     def helper(p: Population, daysLeft: Int): Population =
-      if (daysLeft == 0) p else helper(p.passDay(), daysLeft - 1)
+      if (daysLeft == 0) p else helper(p.tick(), daysLeft - 1)
 
     helper(population, days).sumPopulation
 
 object Day6:
-  case class Population(fishes: Map[Int, Long]):
-    def passDay() =
-      val readyToBorn = fishes.getOrElse(0, 0L)
-      val newPop = fishes.map {
+  case class Population(private var fishes: Map[Int, Long]):
+    fishes = fishes.withDefaultValue(0L)
+
+    def tick() = Population {
+      fishes.map {
         case (0, i) => (8, i)
         case (f, i) => (f - 1, i)
-      }.+(6 -> (fishes.getOrElse(7, 0L) + readyToBorn))
-      Population(newPop)
+      }.+(6 -> (fishes(7) + fishes(0)))
+    }
 
     lazy val sumPopulation = fishes.foldLeft(0L)(_ + _._2)
